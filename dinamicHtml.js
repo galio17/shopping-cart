a923df30a2b57ab7e1582212793f50b41ac56832
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-05-11 19:39:08 -0300

    Creation page

---
+++ dinamicHtml.js
@@ -0,0 +1,47 @@
+function prices() {
+    let totalPrice = document.createElement('div');
+    let total = 0;
+
+    productsCart.forEach(product => {
+        addProduct(product);
+        total += product.price;
+    });
+
+    totalPrice.id = 'total';
+    totalPrice.innerHTML = `<h3>Total</h3> <h3>R$ ${total}</h3>`;
+
+    productDetails.appendChild(totalPrice);
+}
+
+function addProduct({id, name, price}) {
+    const newProduct = document.createElement('li');    
+
+    newProduct.id = id;
+    newProduct.innerHTML = `<p>${name}</p> <p>R$ ${price.toFixed(2)}</p>`;
+
+    productList.appendChild(newProduct);
+}
+
+const main = document.createElement('main');
+const logo = document.createElement('h1');
+const productList = document.createElement('ul');
+const productListHeader = document.createElement('li');
+const productDetails = document.createElement('section');
+const endButton = document.createElement('button');
+
+logo.innerText = 'Virtual Market';
+productListHeader.id = 'list-header';
+productListHeader.innerHTML = '<h2>Item</h2> <h2>Valor</h2>';
+endButton.innerText = 'Finalizar compra';
+
+productList.appendChild(productListHeader);
+
+prices();
+
+productDetails.appendChild(endButton);
+
+main.appendChild(logo);
+main.appendChild(productList);
+main.appendChild(productDetails);
+
+document.querySelector('body').appendChild(main);   
--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const productsCart = [
     {
         id: 1,
-        name: "",
-        price: 0.00
+        name: "Banana",
+        price: 2.00
+    },
+    {
+        id: 2,
+        name: "Maça",
+        price: 3.00
+    },
+    {
+        id: 3,
+        name: "Pera",
+        price: 4.00
+    },
+    {
+        id: 4,
+        name: "Pessego",
+        price: 5.00
+    },
+    {
+        id: 5,
+        name: "Melancia",
+        price: 6.00
+    },
+    {
+        id: 6,
+        name: "Manga",
+        price: 7.00
     }
-]
+];
